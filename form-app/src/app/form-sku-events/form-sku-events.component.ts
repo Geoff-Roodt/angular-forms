df45658cae91929a3f021e1c5c6e8a35f9e15c4c
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-22 12:35:35 +1300

    Added code for event binding to the form

--- form-app/src/app/form-sku-events/form-sku-events.component.ts
+++ form-app/src/app/form-sku-events/form-sku-events.component.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, AbstractControl, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'form-sku-events',
   templateUrl: './form-sku-events.component.html',
   styleUrls: ['./form-sku-events.component.css']
 })
-export class FormSkuEventsComponent implements OnInit {
 
-  constructor() { }
+export class FormSkuEventsComponent implements OnInit {
+  myForm:FormGroup;
+  sku: AbstractControl;
+
+  constructor(fb:FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['', Validators.required]
+    });
+
+    this.sku = this.myForm.controls['sku'];
+    this.sku.valueChanges.subscribe(
+      (value:string) => {
+        console.log('sku changed to: ', value);
+      }
+    );
+
+    this.myForm.valueChanges.subscribe(
+      (form:any) => {
+        console.log('form changed to: ', form);
+      }
+    );
+  }
+  
+  onSubmit(value:string):void{
+    console.log('You submitted value: ', value);
+  }
 
   ngOnInit() {
   }
