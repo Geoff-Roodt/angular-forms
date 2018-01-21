22fd8d82a839fbb2badcccbc534937c64ac9a5d3
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-22 11:36:03 +1300

    Added more code to continue the shorthand validation

--- form-app/src/app/form-sku-shorthand-validation/form-sku-shorthand-validation.component.ts
+++ form-app/src/app/form-sku-shorthand-validation/form-sku-shorthand-validation.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-form-sku-shorthand-validation',
+  selector: 'form-sku-shorthand-validation',
   templateUrl: './form-sku-shorthand-validation.component.html',
   styleUrls: ['./form-sku-shorthand-validation.component.css']
 })
-export class FormSkuShorthandValidationComponent implements OnInit {
 
-  constructor() { }
+export class FormSkuShorthandValidationComponent implements OnInit {
+  myForm:FormGroup;
+
+  constructor(fb:FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['', Validators.required]
+    });
+  }
+
+  onSubmit(value:string):void{
+    console.log('You submitted value: ', value);
+  }
 
   ngOnInit() {
   }
