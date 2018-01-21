b1aa9e09dc7905866de602fc03aee6c8bf2dd232
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-22 10:16:36 +1300

    Added Builder component and form

--- form-app/src/app/app.module.ts
+++ form-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormSkuComponent } from './form-sku/form-sku.component';
+import { FormSkuBuilderComponent } from './form-sku-builder/form-sku-builder.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormSkuComponent
+    FormSkuComponent,
+    FormSkuBuilderComponent
   ],
   imports: [
     BrowserModule,
