ca89fa34798e5c3d37838edab7f4cbdf19734b89
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-22 12:24:06 +1300

    Fixed errors, minor code changes

--- form-app/src/app/form-sku-custom-validation/form-sku-custom-validation.component.ts
+++ form-app/src/app/form-sku-custom-validation/form-sku-custom-validation.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, AbstractControl, FormControl} from '@angular/forms';
+
+function skuValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return {invalidSku: true};
+  }
+}
 
 @Component({
-  selector: 'app-form-sku-custom-validation',
+  selector: 'form-sku-custom-validation',
   templateUrl: './form-sku-custom-validation.component.html',
   styleUrls: ['./form-sku-custom-validation.component.css']
 })
-export class FormSkuCustomValidationComponent implements OnInit {
 
-  constructor() { }
+export class FormSkuCustomValidationComponent implements OnInit {
+  myForm:FormGroup;
+  sku: AbstractControl;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku':  ['', Validators.compose([
+        Validators.required, skuValidator])]
+    });
+    this.sku = this.myForm.controls['sku'];
+  }
+
+  onSubmit(value:string):void{
+    console.log('You submitted value: ', value);
+  }
 
   ngOnInit() {
   }
-
 }
