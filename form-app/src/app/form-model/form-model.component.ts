b1a816ba607e14d13019e6611e8b3d220ee5917e
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-22 12:52:38 +1300

    Added view/logic for 2 way binding product name

--- form-app/src/app/form-model/form-model.component.ts
+++ form-app/src/app/form-model/form-model.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, AbstractControl, FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-form-model',
+  selector: 'form-model',
   templateUrl: './form-model.component.html',
   styleUrls: ['./form-model.component.css']
 })
-export class FormModelComponent implements OnInit {
 
-  constructor() { }
+export class FormModelComponent implements OnInit {
+  myForm:FormGroup;
+  productName:string;
+
+  constructor(fb:FormBuilder) {
+    this.myForm = fb.group({
+      'productName': ['', Validators.required]
+    });
+  }
+
+  onSubmit(value:string):void{
+    console.log('you submitted value: ', value);
+  }
 
   ngOnInit() {
   }
